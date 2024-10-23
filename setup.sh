d9daedfde6a63a4fe9366ae77463e3cdc44be81b
awpsec <uzi@awper.net>
2024-10-23 16:44:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Define the name of the virtual environment
 VENV_DIR="scrapy-env"
 
-# Create virtual environment
 if [ ! -d "$VENV_DIR" ]; then
   echo "Creating virtual environment..."
   python3 -m venv $VENV_DIR
@@ -11,11 +9,9 @@ else
   echo "Virtual environment already exists."
 fi
 
-# Activate the virtual environment
 echo "Activating virtual environment..."
 source "$VENV_DIR/bin/activate"
 
-# Install required dependencies
 if [ -f "requirements.txt" ]; then
   echo "Installing dependencies from requirements.txt..."
   pip install -r requirements.txt
